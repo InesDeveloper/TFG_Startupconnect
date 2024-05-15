60096256cbe2f2154b53fe5ea35ff4e543d74276
InesDeveloper <efreya_sanchez@hotmail.com>
2024-05-15 09:35:37 +0200

    boton que faltaba en usuario

--- startupconnect/php/menuUsuario.php
+++ startupconnect/php/menuUsuario.php
@@ -18,6 +18,7 @@ echo '
                 <li><button class="navButton" onclick="location.href=\'dashboard.php\'">'.$translations['navigation_menu_mis_proyectos'].'</button></li>
                 <li><button class="navButton" onclick="location.href=\'formularioProyecto.php\'">'.$translations['navigation_menu_nuevo_proyecto'].'</button></li>
                 <li><button class="navButton" onclick="location.href=\'contacto.php\'">'.$translations['navigation_menu_soporte'].'</button></li>
+                <li><button class="navButton" onclick="location.href=\'asistenteVirtual.php\'">'.$translations['navigation_asistente'].'</button></li>
                 <li><button class="logout-button" onclick="location.href=\'logout.php\'">'.$translations['navigation_menu_cerrar_sesion'].'</button></li>
             </ul>
             <img id="logo" src="../assets/img/LogoSConnect.png">
